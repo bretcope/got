c50628b5b3c0ed7b8a88a8bc41b3735333159e51
Bret Copeland <bret@atlantisflight.org>
2018-03-28 00:51:22 -0400

    Add UTF-8 helper methods

---
+++ src/Utf8.cpp
@@ -0,0 +1,87 @@
+#include "Utf8.h"
+
+uint32_t Utf8::ExpectedSize(char firstByte)
+{
+    const uint8_t MASK_1 =  0b10000000;
+    const uint8_t MASK_2 =  0b11100000;
+    const uint8_t MASK_3 =  0b11110000;
+    const uint8_t MASK_4 =  0b11111000;
+
+    const uint8_t VALUE_1 = 0b00000000;
+    const uint8_t VALUE_2 = 0b11000000;
+    const uint8_t VALUE_3 = 0b11100000;
+    const uint8_t VALUE_4 = 0b11110000;
+
+    auto uChar = (uint8_t)firstByte;
+
+    if ((uChar & MASK_1) == VALUE_1)
+        return 1;
+
+    if ((uChar & MASK_2) == VALUE_2)
+        return 2;
+
+    if ((uChar & MASK_3) == VALUE_3)
+        return 3;
+
+    if ((uChar & MASK_4) == VALUE_4)
+        return 4;
+
+    return 0;
+}
+
+const uint32_t MAX_1_BYTE = 0x7F;
+const uint32_t MAX_2_BYTE = 0x7FF;
+const uint32_t MAX_3_BYTE = 0xFFFF;
+const uint32_t MAX_4_BYTE = 0x10FFFF;
+
+uint32_t Utf8::EncodedSize(char32_t ch)
+{
+    if (ch <= MAX_1_BYTE)
+        return 1;
+
+    if (ch <= MAX_2_BYTE)
+        return 2;
+
+    if (ch <= MAX_3_BYTE)
+        return 3;
+
+    if (ch <= MAX_4_BYTE)
+        return 4;
+
+    return 0;
+}
+
+uint32_t Utf8::Encode(char32_t ch, char* buffer)
+{
+    if (ch <= MAX_1_BYTE)
+    {
+        *buffer = (uint8_t)ch;
+        return 1;
+    }
+
+    if (ch <= MAX_2_BYTE)
+    {
+        buffer[0] = (uint8_t)(0b11000000u | (ch >> 6u));
+        buffer[1] = (uint8_t)(0b10000000u | (ch & 0x3Fu));
+        return 2;
+    }
+
+    if (ch <= MAX_3_BYTE)
+    {
+        buffer[0] = (uint8_t)(0b11100000u | (ch >> 12u));
+        buffer[1] = (uint8_t)(0b10000000u | ((ch >> 6u) & 0x3Fu));
+        buffer[2] = (uint8_t)(0b10000000u | (ch & 0x3Fu));
+        return 3;
+    }
+
+    if (ch <= MAX_4_BYTE)
+    {
+        buffer[0] = (uint8_t)(0b11110000u | (ch >> 24u));
+        buffer[1] = (uint8_t)(0b10000000u | ((ch >> 12u) & 0x3Fu));
+        buffer[2] = (uint8_t)(0b10000000u | ((ch >> 6u) & 0x3Fu));
+        buffer[3] = (uint8_t)(0b10000000u | (ch & 0x3Fu));
+        return 4;
+    }
+
+    return 0;
+}
