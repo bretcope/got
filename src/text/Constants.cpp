fc22c03ef6a61064b82802bfe71aa7f80f567514
Bret Copeland <bret@atlantisflight.org>
2018-04-13 22:07:04 -0600

    Add namespace of string constants

---
+++ src/text/Constants.cpp
@@ -0,0 +1,22 @@
+#include "Constants.h"
+
+namespace mot
+{
+    namespace Constants
+    {
+        const MotString ALIAS("alias");
+        const MotString CONFIG("config");
+        const MotString DEFAULT("default");
+        const MotString ENV("env");
+        const MotString FILE("file");
+        const MotString INCLUDE("include");
+        const MotString OVERRIDE("override");
+        const MotString PATH("path");
+        const MotString PREFIX("prefix");
+        const MotString PROFILE("profile");
+        const MotString REMOTE("remote");
+        const MotString RENAME("rename");
+        const MotString REPO("repo");
+        const MotString RESOURCE("resource");
+    }
+}
---
+++ src/text/Constants.h
@@ -0,0 +1,27 @@
+#ifndef MOT_CONSTANTS_H
+#define MOT_CONSTANTS_H
+
+#include "MotString.h"
+
+namespace mot
+{
+    namespace Constants
+    {
+        extern const MotString ALIAS;
+        extern const MotString CONFIG;
+        extern const MotString DEFAULT;
+        extern const MotString ENV;
+        extern const MotString FILE;
+        extern const MotString INCLUDE;
+        extern const MotString OVERRIDE;
+        extern const MotString PATH;
+        extern const MotString PREFIX;
+        extern const MotString PROFILE;
+        extern const MotString REMOTE;
+        extern const MotString RENAME;
+        extern const MotString REPO;
+        extern const MotString RESOURCE;
+    }
+}
+
+#endif //MOT_CONSTANTS_H
